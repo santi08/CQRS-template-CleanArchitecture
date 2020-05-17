409a57ced4bf169d779b3b2f533a17e5139b16c4
Santiago Guarin <santiaristi_1994@hotmail.com>
2020-05-17 15:49:46 -0500

    Implementing DbContext

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using Domain;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Persistence
+{
+    public class DataContext : IdentityDbContext<AppUser>
+    {
+        public DataContext(DbContextOptions options)
+            : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
