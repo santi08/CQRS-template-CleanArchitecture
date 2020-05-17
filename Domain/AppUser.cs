d84c0d5d9a93532c6ab90d6ac7b2ae4777f287c2
Santiago Guarin <santiaristi_1994@hotmail.com>
2020-05-17 15:43:22 -0500

    Add User class

---
+++ Domain/AppUser.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain
+{
+    public class AppUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
